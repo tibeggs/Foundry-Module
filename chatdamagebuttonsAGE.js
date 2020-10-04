ed7f9e6b27c1d17dad8226d2af753e6feb97d568
tibeggs <tibeggs@gmail.com>
2020-10-03 23:11:08 -0400

    hh

--- chatdamagebuttonsAGE.js
+++ chatdamagebuttonsAGE.js
@@ -3,29 +3,29 @@ class ChatDamageButtons5e extends Application {
         super(app);
     }
 
-    init () {
+    init() {
 
-        Hooks.on('renderChatMessage', (message, html, data) => {            
-
+        Hooks.on('renderChatMessage', (message, html, data) => {
+            console.log(message);
             if (message.isRoll || message.roll.parts[0].faces == 6) return
-            
-            
-                let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-                const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply calculate damage minus armor."></i></button>`);
 
-                const btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
-                btnContainer.append(fullDamageButton);
+            let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-                html.find('.dice-total').append(btnContainer);
+            const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply calculate damage minus armor."></i></button>`);
 
-                // Handle button clicks
-                fullDamageButton.click(ev => {
-                    ev.stopPropagation();
-                    console.log(CONFIG.Actor.entityClass);
-                });
-                
-            
+            const btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
+            btnContainer.append(fullDamageButton);
+
+            html.find('.dice-total').append(btnContainer);
+            console.log("poop");
+            // Handle button clicks
+            fullDamageButton.click(ev => {
+                ev.stopPropagation();
+                console.log(CONFIG.Actor.entityClass);
+            });
+
+
         })
     }
 }
