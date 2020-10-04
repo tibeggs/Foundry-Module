7736fac97335f19e6645526e89bae113ad3c12b3
tibeggs <tibeggs@gmail.com>
2020-10-04 00:01:16 -0400

    Working

--- chatdamagebuttonsAGE.js
+++ chatdamagebuttonsAGE.js
@@ -7,25 +7,51 @@ class ChatDamageButtons5e extends Application {
 
         Hooks.on('renderChatMessage', (message, html, data) => {
             console.log(message);
-            if (message.isRoll || message.roll.parts[0].faces == 6) 
+            if (true != false) {
+                console.log("poop");
 
 
-            let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
+                let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-            const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply calculate damage minus armor."></i></button>`);
+                let fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply calculate damage minus armor."></i></button>`);
 
-            const btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
-            btnContainer.append(fullDamageButton);
+                let btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
+                btnContainer.append(fullDamageButton);
 
-            html.find('.dice-total').append(btnContainer);
-            console.log("poop");
-            // Handle button clicks
-            fullDamageButton.click(ev => {
-                ev.stopPropagation();
-                console.log(CONFIG.Actor.entityClass);
-            });
+                html.find('.dice-total').append(btnContainer);
+
+                // Handle button clicks
+                fullDamageButton.click(ev => {
+                    ev.stopPropagation();
+                    console.log(_token);
+                    if (canvas.tokens.controlled.length == 0 || canvas.tokens.controlled.length > 1) {
+                        ui.notifications.error("Please select a single token");
+                    }
+                    let target = canvas.tokens.controlled[0].actor;
+
+                    const emptyPhysicalItem = {
+                        type: "Armor",
+                        description: "",
+                        quantity: 1,
+                        weight: 0,
+                    };
 
 
+                    const emptyArmor = Object.assign(Object.assign({}, emptyPhysicalItem), { type: "Armor", description: "", armorPenalty: 0, armorRating: 0, equipped: false });
+
+                    const armor = (target.items || [])
+                        .filter((i) => i.data.data.type == "Armor")
+                        .filter((i) => i.data.data.equipped)
+                        .map((i) => i.data)
+                        .reduce((c, v) => (c.armorRating > v.armorRating ? c : v), emptyArmor);
+
+
+                    if (armor.data.armorRating != undefined) {
+                        console.log(armor.data.armorRating);
+                    }
+                });
+
+            }
         })
     }
 }
